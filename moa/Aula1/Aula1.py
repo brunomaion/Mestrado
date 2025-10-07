18cf53d20fc91b1c03ef724189d79a0cf3925e6c
brunomaion <brunosmaion@hotmail.com>
2025-10-07 09:44:42 -0300

    3

--- moa/Aula1/Aula1.py
+++ moa/Aula1/Aula1.py
@@ -4,11 +4,10 @@ import random
 
 def fun_obj(x, y):
   sol = 0.97 * math.exp(-(((x + 3) ** 2 + (y + 3) ** 2) / 5)) \
-  + 0.98 * math.exp(-(((x + 3) ** 2 + (y + 3) ** 2) / 5)) \
-  + 0.99 * math.exp(-(((x + 3) ** 2 + (y + 3) ** 2) / 5)) \
-  + 1 * math.exp(-(((x + 3) ** 2 + (y + 3) ** 2) / 5))
+      + 0.98 * math.exp(-(((x - 3) ** 2 + (y + 3) ** 2) / 5)) \
+      + 0.99 * math.exp(-(((x + 3) ** 2 + (y - 3) ** 2) / 5)) \
+      + 1.00 * math.exp(-(((x - 3) ** 2 + (y - 3) ** 2) / 5))
   return sol
-
 
 def rand():
   return random.uniform(0, 1)
@@ -85,10 +84,12 @@ def busca_iterativa_local_rapida(range_0, range_1, parada):
     #Explotação
     j=1
     while (j!=0):
-      sol_corrente=explotacao_min(sol_corrente[0], sol_corrente[1], sol_valor, 0.1)
-      if fun_obj(sol_corrente[0], sol_corrente[1]) < sol_valor:
-        sol = sol_corrente
-        sol_valor = fun_obj(sol[0], sol[1])
+      t = explotacao_min(sol_corrente[0], sol_corrente[1], sol_valor, 0.1)
+      if fun_obj(t[0], t[1]) < fun_obj(sol_corrente[0], sol_corrente[1]):
+        sol_corrente = t
+        if fun_obj(t[0], t[1]) < fun_obj(sol[0], sol[1]):
+          sol=t
+          sol_valor=fun_obj(t[0], t[1])
       else:
         j=0
     #Exploração
@@ -96,6 +97,7 @@ def busca_iterativa_local_rapida(range_0, range_1, parada):
 
   print("Solução: ", sol)
   print("Valor: ", sol_valor)
+  print("t:", t)
 
 
 
@@ -109,3 +111,11 @@ print(teste)
 
 teste = fun_obj(4, 4)
 print(teste)
+
+teste = fun_obj(3, -3)
+print(teste)
+
+teste = fun_obj(4, 4)
+print(teste)
+
+
